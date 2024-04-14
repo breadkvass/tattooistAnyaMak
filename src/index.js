abd406868953985874f711bee2b9ea66ac11ab52
Anastasiya <sulimova.a.m@yandex.ru>
2024-04-14 20:14:52 +0300

    add spinner loader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import './index.css';
 import Glide from '@glidejs/glide'
 
-new Glide('.glide').mount()
+window.addEventListener('DOMContentLoaded', function () {
+    new Glide('.glide').mount()
 
-const glide = new Glide('#intro', {
-    type: 'carousel',
-    startAt: 0,
-    perView: 3,
-    gap: 5
+    const glide = new Glide('#intro', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 3,
+        gap: 5
+    })
+    
+    glide.mount();
 })
-  
-glide.mount()
