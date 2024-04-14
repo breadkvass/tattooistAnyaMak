8df21d7031072eb5edead22f3b4fa6b5ba0016ac
Anastasiya <sulimova.a.m@yandex.ru>
2024-04-14 19:00:22 +0300

    add glide func

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './index.css';
+import Glide from '@glidejs/glide'
+
+new Glide('.glide').mount()
+
+const glide = new Glide('#intro', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 3,
+    gap: 5
+})
+  
+glide.mount()
